3c8def1806297cb10aec4527c724058bc5f5d708
Olger Elias Garavito Muñoz <ogaravito@pedagogica.edu.co>
2021-07-16 13:05:33 -0500

    Ejemplos de iteraciones

---
+++ ordenamiento.py
@@ -0,0 +1,44 @@
+"""
+╔════════════════════════════════╗
+║  ALGORITMOS DE ORDENAMIENTO    ║
+╠════════════════════════════════╣
+║ Burbuja                        ║
+║ Selección                      ║
+║ Inserción                      ║
+║ Inserción Binaria              ║
+║ Shell                          ║
+║ Rápido                         ║
+║ Merge                          ║
+╚════════════════════════════════╝
+"""
+
+def BubleSort(lista):
+    n=len(lista)
+    k=1
+    for i in range(n-1):
+        for j in range(i+1,n):
+            if lista[i]>lista[j]:
+                lista[i],lista[j]=lista[j],lista[i]
+            k+=1
+    return lista
+
+
+def SelectionSort(lista):
+    n=len(lista)
+    for i in range(n):
+        menor=i
+        for j in range(i+1,n):
+            if lista[j]<lista[menor]:
+                menor=j
+        lista[i],lista[menor] = lista[menor],lista[i]
+    return lista
+
+def insertionSort(lista):
+    for i in range(1, len(lista)):
+        num = lista[i]
+        j = i-1
+        while j >=0 and num < lista[j] :
+                lista[j+1] = lista[j]
+                j -= 1
+        lista[j+1] = num
+    return lista
---
+++ piramide.py
@@ -0,0 +1,9 @@
+num = 15
+for i in range(1,num+1):
+	for j in range(1,num-i+1):
+		print(end="\t")
+	for j in range(i,0,-1):
+		print(j,end="\t")
+	for j in range(2,i+1):
+		print(j,end="\t")
+	print()
